b46d52498f8cef211c7cf65c3b5e6dc5c891a865
Wilson Chan <wilsonchan54@gmail.com>
2024-04-05 23:14:41 -0400

    chore: Create list of sidebar links as array in constants index.ts / render sidebarLinks array with map in Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,21 +1,48 @@
+'use client'
+
 import React from 'react'
+import { sidebarLinks } from '@/constants/index'
+import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
 type Props = {}
 
 const Sidebar = (props: Props) => {
+  const pathname = usePathname()
+
   return (
     <section className="
       bg-orange-700 text-white sticky left-0 top-0 flex h-screen w-fit 
-      flex-col justify-start p-5 pt-24 max-sm:hidden lg:w-[250px]"
+      flex-col justify-start p-5 pt-10 max-sm:hidden lg:w-[250px]"
     >
-      <h1 className='bg-green-700 text-5xl mb-10'>Sidebar Here</h1>
+      <h1 className='bg-green-700 text-3xl mb-10'>Sidebar Here</h1>
       {/* List of links */}
-      <div className="bg-blue-700 flex flex-col gap-4">
-        <p>item 1</p>
-        <p>item 2</p>
-        <p>item 3</p>
-        <p>item 4</p>
-        <p>item 5</p>
+      <div className="bg-blue-700 flex flex-col gap-2">
+        {sidebarLinks.map((item) => {
+          const isActive = pathname === item.route || pathname.startsWith(item.route)
+
+          return(
+            <Link 
+              href={item.route}
+              key={item.label}
+              className={cn('flex gap-4 items-center p-5 rounded-xl justify-start', 
+                {'bg-purple-400' : isActive,}
+              )}
+            >
+              <Image 
+                src={item.imgUrl}
+                alt={item.label}
+                width={50}
+                height={50}
+              />
+              <p>
+                {item.label}
+              </p>
+            </Link>
+          )
+        })}
       </div>
     </section>
   )
---
+++ constants/index.ts
@@ -0,0 +1,33 @@
+
+export const sidebarLinks = [
+  {
+    label: 'Home',
+    route: '/',
+    imgUrl: './vercel.svg',
+  },
+  {
+    label: 'Upcoming',
+    route: '/upcoming',
+    imgUrl: './vercel.svg',
+  },
+  {
+    label: 'Previous',
+    route: '/previous',
+    imgUrl: './vercel.svg',
+  },
+  {
+    label: 'Recording',
+    route: '/recording',
+    imgUrl: './vercel.svg',
+  },
+  {
+    label: 'Personal Room',
+    route: '/personal-room',
+    imgUrl: './vercel.svg',
+  },
+  // {
+  //   label: '',
+  //   route: '',
+  //   imgUrl: '',
+  // },
+]
