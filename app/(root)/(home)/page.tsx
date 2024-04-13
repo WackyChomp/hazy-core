dd0d769c0ad9e61df70c2b89e68f390bceb5fdc9
Wilson Chan <wilsonchan54@gmail.com>
2024-04-13 10:59:05 -0400

    chore: Rename next.config extension and add remote image sources / create custom glassmorphism in global.css / remove default color properties in tailwind.config / create hero banner in home page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,6 +8,17 @@ const Home = (props: Props) => {
       <h1 className="text-3xl font-bold">
         Home Page
       </h1>
+      <div className="bg-hero_two bg-cover h-[300px] w-full rounded-[20px]">
+        <div className="flex h-full flex-col justify-between max-md:px-10 max-md:py-20 p-7">
+          <h2 className='glassmorphism max-w-[300px] rounded-xl py-2 text-center text-base font-normal'>
+            [Placeholder] Upcoming Meeting at: 3:45 PM
+          </h2>
+          <div className="glassmorphism max-w-[350px] p-2 rounded-xl flex flex-col gap-3">
+            <h1 className='text-4xl font-semibold lg:text-7xl'>11:30 AM</h1>
+            <p className='text-lg font-medium lg:text-2xl'>Wednesday, April 20, 20XX</p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,44 +23,10 @@ const config = {
           1:'#1C1F2E',
           2:'#161925',
         },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      backgroundImage:{
+        hero_one: "url('https://media4.giphy.com/media/10QZvWwBUQbpqE/giphy.gif')",
+        hero_two: "url('https://media1.giphy.com/media/SExu1NjbmGALSv5Bm2/200w.gif?cid=6c09b9527h2ku0dkcdu4bks12ffzplk99mcflh0k1rkchy9t&ep=v1_gifs_search&rid=200w.gif&ct=g')"
       },
       keyframes: {
         "accordion-down": {
