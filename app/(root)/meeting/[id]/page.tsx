b8abf05a2bcaeef053deff21fec40155f6d24a31
Wilson Chan <wilsonchan54@gmail.com>
2024-04-24 19:14:49 -0400

    Create useGetCallById hook function for meeting id page.tsx / destructure 'id' and call useGetCallById hook function in meeting id page.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,13 +1,24 @@
+'use client'
 
 import { useUser } from '@clerk/nextjs'
 import React, { useState} from 'react'
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import MeetingRoom from '@/components/MeetingRoom';
 import MeetingSetup from '@/components/MeetingSetup';
+import { useGetCallById } from '@/hook/useGetCallById';
+import Loader from '@/components/Loader';
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({ params: {id} }: { params: { id: string } }) => {
   const { user, isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false)
+
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if(!isLoaded || !isCallLoading) return <Loader />
+
+  if(!call) return(
+    <p className="text-green-400">No call has been found</p>
+  )
 
   return (
     <main className="h-screen w-full">
@@ -20,7 +31,7 @@ const Meeting = ({ params }: { params: { id: string } }) => {
           )}
         </StreamTheme>
       </StreamCall>
-    <div>Meeting Room # {params.id} </div>
+    <div>Meeting Room # {id} </div>
     </main>
   )
 }
