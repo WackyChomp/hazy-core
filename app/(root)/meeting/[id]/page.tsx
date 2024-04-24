3209fe65876b6301e6cb0cd5be7a6b8f69532008
Wilson Chan <wilsonchan54@gmail.com>
2024-04-23 21:58:08 -0400

    chore: Create MeetingSetup and MeetingRoom components placeholder for meeting id page.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+
+import { useUser } from '@clerk/nextjs'
+import React, { useState} from 'react'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
 
 const Meeting = ({ params }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false)
+
   return (
+    <main className="h-screen w-full">
+      <StreamCall>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup />      // waiting room
+          ):(
+            <MeetingRoom />       // 
+          )}
+        </StreamTheme>
+      </StreamCall>
     <div>Meeting Room # {params.id} </div>
+    </main>
   )
 }
 
---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const MeetingSetup = (props: Props) => {
+  return (
+    <div>MeetingSetup</div>
+  )
+}
+
+export default MeetingSetup
