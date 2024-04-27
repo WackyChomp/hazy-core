427fa624496919ccf62453616d9acc85f607b285
Wilson Chan <wilsonchan54@gmail.com>
2024-04-26 22:03:40 -0400

    Add Button component, ability to toggle VideoPreview component, and if conditional for useCall component in MeetingSetup component

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
+import { Button } from './ui/button'
 import { useCall, VideoPreview } from '@stream-io/video-react-sdk'
+import { DeviceSettings } from '@stream-io/video-react-sdk'
 
 
 /*
@@ -17,6 +19,10 @@ const MeetingSetup = (props: Props) => {
 
   const call = useCall();
 
+  if(!call){
+    throw new Error ('useCall must be used within StreamCall component')
+  }
+
   useEffect(() => {
     if(isMicAndCamToggledOn){
       call?.camera.disable();
@@ -31,10 +37,26 @@ const MeetingSetup = (props: Props) => {
   
 
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-orange-500'>
+    <div className='bg-blue-800 flex h-screen w-[400px] flex-col items-center justify-center gap-3 text-orange-500'>
       <h1 className='text-2xl font-bold'>MeetingSetup</h1>
 
       <VideoPreview />
+
+      <div className="border border-yellow-400 flex h-20 items-center justify-center gap-5">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input 
+            type="checkbox" 
+            checked={isMicAndCamToggledOn}
+            onChange={(e) => setisMicAndCamToggledOn(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        <DeviceSettings />
+      </div>
+
+      <Button>
+        Join Meeting
+      </Button>
 
     </div>
   )
